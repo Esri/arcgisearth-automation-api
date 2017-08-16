b4ffe6d39ea66c6db21c323cbe92e46a426c28a7
ben7276 <btan@esri.com>
2017-08-16 14:12:14 +0800

    add test client for arcgis earth automation api

---
+++ EarthAPITestClient/EarthAPITestClient/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EarthAPITestClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel v = new ViewModel();
+            this.DataContext = v;
+        }
+
+        private void GetCameraButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+             v.ExecuteFuction(FunctionType.GetCamera);
+        }
+
+        private void SetCameraButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.SetCamera);
+        }
+
+        private void FlyToButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.FlyTo);
+        }
+
+        private void AddLayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.AddLayer);
+        }
+
+        private void ClearLayersButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.ClearLayers);
+        }
+
+        private void GetSnapshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.GetSnapshot);
+        }
+
+        private void ConnectEarth_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.Connect);
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.Help);
+        }
+
+        private void ClearTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.ClearTextBox);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            ViewModel v = this.DataContext as ViewModel;
+            v.ExecuteFuction(FunctionType.CloseConnect);
+        }
+    }
+}
