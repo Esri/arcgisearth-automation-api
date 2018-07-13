b8a49a187749a9bea0d5f864c1159631ff5f3319
ben7276 <btan@esri.com>
2018-07-13 13:58:55 +0800

    update contract

--- EarthAPITestClient/EarthAPIUtils/IEarthNamedpipeService.cs
+++ EarthAPITestClient/EarthAPIUtils/IEarthNamedpipeService.cs
@@ -57,7 +57,7 @@ namespace ArcGISEarth.WCFNamedPipeIPC
     /// The public interface for communicating with ArcGIS Earth Automation API.
     /// </summary>
     [ServiceContract(
-        Namespace = "ArcGISEarth/2018/02", 
+        Namespace = "ArcGISEarth/2018/07",
         CallbackContract = typeof(IEarthNamedpipeCallbackService))]
     public interface IEarthNamedpipeService
     {
@@ -136,7 +136,6 @@ namespace ArcGISEarth.WCFNamedPipeIPC
         /// <para>"SceneService" – ArcGIS scene service.</para>
         /// <para>"Raster" – Local elevation raster formats.</para>
         /// <para>"TileLayerPackage" – ArcGIS tile layer package.</para>
-        /// <para>"ElevationService" – ArcGIS elevation service.</para>
         /// <para>
         /// The URI(URIs) value specifies the URL or path of a layer. Use URIs if the source files are multiple elevation source and used as elevation.
         /// </para>
@@ -148,69 +147,53 @@ namespace ArcGISEarth.WCFNamedPipeIPC
         /// <para>"ElevationLayers" – layers added into Terrain pane of ArcGIS Earth as elevation.</para>
         /// </para>
         /// </param>
-        //[FaultContract(typeof(EarthNamedpipeFault))]
-        //[OperationContract]
-        //void AddLayerAsync(string json);
-
-        /// <summary>
-        /// Add layer to the table of contents, basemap or terrain of ArcGIS Earth.
-        /// <remarks>
-        /// <para>
-        /// Duplex communication is used when adding layer. IEarthNamedpipeCallbackService interface need to be implemented.
-        /// Then client side can receive result of adding layer from Notify callback function. 
-        /// </para>
-        /// </remarks>
-        /// </summary>
-        /// <param name="json">is a string in JSON format contains layer type, URI and target of layer
-        /// <para>
-        /// The JSON string parameter can have fields: "type"(optional), "target"(optional) and URI(or URIs).
-        /// </para>
-        /// <para>
-        /// The type value specifies type supported in ArcGIS Earth. The API will automatically set an appropriate type according to URI if the string doesn't contain it.
-        /// </para>
-        /// <para>And the value can be one of these: </para>
-        /// <para>"FeatureService" – ArcGIS feature service.</para>
-        /// <para>"MapService" – ArcGIS map service.</para>
-        /// <para>"ImageService" – ArcGIS image service.</para>
-        /// <para>"Shapefile" – Local shapefile.</para>
-        /// <para>"OGCWMS" – OGC Web Map Service</para>
-        /// <para>"KML" – Local KML and KMZ.</para>
-        /// <para>"SceneLayerPackage" – ArcGIS scene layer package.</para>
-        /// <para>"SceneService" – ArcGIS scene service.</para>
-        /// <para>"Raster" – Local elevation raster formats.</para>
-        /// <para>"TileLayerPackage" – ArcGIS tile layer package.</para>
-        /// <para>"ElevationService" – ArcGIS elevation service.</para>
-        /// <para>
-        /// The URI(URIs) value specifies the URL or path of a layer. Use URIs if the source files are multiple elevation source and used as elevation.
-        /// </para>
-        /// <para>
-        /// The target value specifies the target place where the layers added to. If not specified, the default value is "OperationalLayers".
-        /// <para>The value can one of these:</para>
-        /// <para>"OperationalLayers" - layers added into the table of contents of ArcGIS Earth.</para>
-        /// <para>"BasemapLayers" - layers added into Basemap pane of ArcGIS Earth as basemap.</para>
-        /// <para>"ElevationLayers" – layers added into Terrain pane of ArcGIS Earth as elevation.</para>
-        /// </para>
-        /// </param>
-        /// <returns>String indicating the id of layer.
+        /// <returns>String indicating the id of layer.</returns>
         [FaultContract(typeof(EarthNamedpipeFault))]
         [OperationContract]
         string AddLayer(string json);
 
+        /// <summary>
+        /// Get load status of layer.
+        /// </summary>
+        /// <param name="json">is a string in JSON format contains id of layer 
+        /// <remarks>
+        /// The JSON string requires fields of "id" which value is the string type id of layer. 
+        /// </remarks>
+        /// </param>
+        /// <returns>String indicating the load status of layer.</returns>
         [FaultContract(typeof(EarthNamedpipeFault))]
         [OperationContract]
         string GetLayerLoadStatus(string json);
 
+        /// <summary>
+        /// Remove a layer.
+        /// </summary>
+        /// <param name="json">is a string in JSON format contains id of layer 
+        /// <remarks>
+        /// The JSON string requires fields of "id" which value is the string type id of layer. 
+        /// </remarks>
+        /// </param>
         [FaultContract(typeof(EarthNamedpipeFault))]
         [OperationContract]
         void RemoveLayer(string json);
 
-        /// <returns>String indicating the json description of all contents in the workspace.
-        /// might need a get content to get the bookmark
+        /// <summary>
+        /// Get the JSON description of current workspace.
+        /// </summary>
+        /// <returns>String indicating the JSON description of current workspace.</returns>
         [FaultContract(typeof(EarthNamedpipeFault))]
         [OperationContract]
         string GetWorkspace();
 
-        // { "operational_layers":{}, "basemaps":{}, "surface":{}, "bookmarks":{}}
+        /// <summary>
+        /// Import workspace to ArcGIS Earth.
+        /// </summary>
+        /// <param name="json">is a string in JSON format of ArcGIS Earth workspace 
+        /// <remarks>
+        /// <para>The JSON string parameters can have fields as "operationalLayers", "elevationLayers", "basemapLayers" and "bookmarks"</para>
+        /// <para>Each value indicating the JSON desciption of each component of workspace"</para>
+        /// </remarks>
+        /// </param>
         [FaultContract(typeof(EarthNamedpipeFault))]
         [OperationContract]
         void ImportWorkspace(string json);
