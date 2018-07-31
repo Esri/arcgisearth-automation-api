423a52915493d8047f6a25b7c96b751c2a84feea
ben7276 <btan@esri.com>
2018-07-31 16:11:27 +0800

    clear up

--- EarthAPITestClient/EarthRESTRouterTestClient/config.py
+++
@@ -1,9 +0,0 @@
-base_address = "http://localhost:80/Temporary_Listen_Addresses/arcgisearth/api/v1"
-
-addLayerEndpoint = '/layer'
-addLayersEndpoint = '/layers'
-
-urlField = 'url'
-urlsField = 'urls'
-layerTypeField = 'type'
-addToField = 'addTo'
