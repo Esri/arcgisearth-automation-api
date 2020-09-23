98a90f64b5990c911cc4ded2aa280c02f35e1dac
xuew8412 <xuewen_zhang@esri.com>
2020-09-23 10:54:44 +0800

    update the Utils

--- src/ArcGISProToArcGISEarth/ToolHelper.cs
+++ src/ArcGISProToArcGISEarth/ToolHelper.cs
@@ -26,9 +26,7 @@ namespace ToArcGISEarth
     public static class ToolHelper
     {
         // ArcGIS Earth automation api utils.
-        public static AutomationAPIHelper Utils { get; } = new AutomationAPIHelper 
-        {
-        };
+        public static AutomationAPIHelper Utils = new AutomationAPIHelper();
                
         // Logging id and it's infomation when adding layer or elevation source.
         public static Dictionary<string, string[]> IdInfoDictionary { get; set; } = new Dictionary<string, string[]>();
