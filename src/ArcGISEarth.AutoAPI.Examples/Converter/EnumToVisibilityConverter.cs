ef3c7c1271740ddd2b38490718d163a66360bf99
xuew8412 <xuewen_zhang@esri.com>
2020-09-24 18:13:25 +0800

    remove system.drawing.common package for .net core solution and display snapshot in the UI directly.

--- src/ArcGISEarth.AutoAPI.Examples/Converter/EnumToVisibilityConverter.cs
+++ src/ArcGISEarth.AutoAPI.Examples/Converter/EnumToVisibilityConverter.cs
@@ -1,32 +1,44 @@
-﻿using System;
+﻿// Copyright 2020 Esri
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
-using static ArcGISEarth.AutoAPI.Examples.MainWindowViewModel;
 
 namespace ArcGISEarth.AutoAPI.Examples.Converter
 {
-    public class EnumToVisibilityConverter : IValueConverter
+    public class EnumToButtonVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var sendButtonType = (SendButtonType)value;
+            var sendButtonType = (FunctionType)value;
             var visibility = Visibility.Visible;
             switch (sendButtonType)
             {
-                case SendButtonType.GetCamera:
-                case SendButtonType.GetWorkspace:
-                case SendButtonType.ClearWorkspace:
+                case FunctionType.GetCamera:
+                case FunctionType.GetWorkspace:
+                case FunctionType.ClearWorkspace:
+                case FunctionType.TakeSnapshot:
                     visibility = Visibility.Visible;
                     break;
-                case SendButtonType.SetCamera:                    
-                case SendButtonType.SetFlight:                    
-                case SendButtonType.AddLayer:
-                case SendButtonType.GetLayer:
-                case SendButtonType.RemoveLayer:
-                case SendButtonType.ClearLayers:
-                case SendButtonType.ImportWorkspace:
-                case SendButtonType.TakeSnapshot:
+                case FunctionType.SetCamera:                    
+                case FunctionType.SetFlight:                    
+                case FunctionType.AddLayer:
+                case FunctionType.GetLayer:
+                case FunctionType.RemoveLayer:
+                case FunctionType.ClearLayers:
+                case FunctionType.ImportWorkspace:
                     visibility = Visibility.Collapsed;
                     break;
             }
@@ -43,23 +55,57 @@ namespace ArcGISEarth.AutoAPI.Examples.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var sendButtonType = (SendButtonType)value;
+            var sendButtonType = (FunctionType)value;
             var visibility = Visibility.Collapsed;
             switch (sendButtonType)
             {
-                case SendButtonType.GetCamera:
-                case SendButtonType.GetWorkspace:
-                case SendButtonType.ClearWorkspace:
+                case FunctionType.GetCamera:
+                case FunctionType.GetWorkspace:
+                case FunctionType.ClearWorkspace:
+                case FunctionType.TakeSnapshot:
                     visibility = Visibility.Collapsed;
                     break;
-                case SendButtonType.SetCamera:
-                case SendButtonType.SetFlight:
-                case SendButtonType.AddLayer:
-                case SendButtonType.GetLayer:
-                case SendButtonType.RemoveLayer:
-                case SendButtonType.ClearLayers:
-                case SendButtonType.ImportWorkspace:
-                case SendButtonType.TakeSnapshot:
+                case FunctionType.SetCamera:
+                case FunctionType.SetFlight:
+                case FunctionType.AddLayer:
+                case FunctionType.GetLayer:
+                case FunctionType.RemoveLayer:
+                case FunctionType.ClearLayers:
+                case FunctionType.ImportWorkspace:
+                    visibility = Visibility.Visible;
+                    break;
+            }
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EnumToImageVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sendButtonType = (FunctionType)value;
+            var visibility = Visibility.Collapsed;
+
+            switch (sendButtonType)
+            {
+                case FunctionType.GetCamera:
+                case FunctionType.GetWorkspace:
+                case FunctionType.ClearWorkspace:
+                case FunctionType.SetCamera:
+                case FunctionType.SetFlight:
+                case FunctionType.AddLayer:
+                case FunctionType.GetLayer:
+                case FunctionType.RemoveLayer:
+                case FunctionType.ClearLayers:
+                case FunctionType.ImportWorkspace:
+                    visibility = Visibility.Collapsed;
+                    break;
+                case FunctionType.TakeSnapshot:
                     visibility = Visibility.Visible;
                     break;
             }
--- src/ArcGISEarth.AutoAPI.Shared/AutomationAPIHelper.cs
+++ src/ArcGISEarth.AutoAPI.Shared/AutomationAPIHelper.cs
@@ -13,11 +13,12 @@
 
 using Newtonsoft.Json.Linq;
 using System;
-using System.Drawing;
 using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ArcGISEarth.AutoAPI.Utils
 {
@@ -199,45 +200,73 @@ namespace ArcGISEarth.AutoAPI.Utils
             }
         }
 
-        public async Task<string> TakeSnapshot(string imagePath)
+        //public async Task<string> TakeSnapshot(string imagePath)
+        //{
+        //    try
+        //    {
+        //        string snapshotRequestUrl = $"{APIBaseUrl}/{SnapshotControllerName}";
+        //        var uri = new Uri(imagePath);
+        //        if (uri.IsAbsoluteUri && uri.IsFile)
+        //        {
+        //            var ext = Path.GetExtension(imagePath);
+        //            if (string.IsNullOrEmpty(ext))
+        //            {
+        //                imagePath += ".jpg";
+        //            }
+        //            ext = Path.GetExtension(imagePath).ToLower();
+        //            if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".tif" || ext == ".tiff")
+        //            {
+        //                HttpClient httpClient = new HttpClient();
+        //                HttpResponseMessage responseMessage = await httpClient.GetAsync(snapshotRequestUrl);
+        //                HttpContent content = responseMessage.Content;
+        //                using (Stream stream = await content.ReadAsStreamAsync())
+        //                {
+        //                    var image = Image.FromStream(stream);
+        //                    image.Save(imagePath);
+        //                }
+
+        //                return "Take snapshot successfully: " + imagePath + ".";
+        //            }
+        //            else
+        //            {
+        //                return "Take snapshot failed, please try to input a correct image format.";
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return "Take snapshot failed, please input a correct file path.";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return ex.Message;
+        //    }
+        //}
+
+        public async Task<ImageSource> TakeSnapshot()
         {
             try
             {
                 string snapshotRequestUrl = $"{APIBaseUrl}/{SnapshotControllerName}";
-                var uri = new Uri(imagePath);
-                if (uri.IsAbsoluteUri && uri.IsFile)
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage responseMessage = await httpClient.GetAsync(snapshotRequestUrl);
+                HttpContent content = responseMessage.Content;
+
+                BitmapImage biImg = new BitmapImage();
+                using (Stream stream = await content.ReadAsStreamAsync())
                 {
-                    var ext = Path.GetExtension(imagePath);
-                    if (string.IsNullOrEmpty(ext))
-                    {
-                        imagePath += ".jpg";
-                    }
-                    ext = Path.GetExtension(imagePath).ToLower();
-                    if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".tif" || ext == ".tiff")
-                    {
-                        HttpClient httpClient = new HttpClient();
-                        HttpResponseMessage responseMessage = await httpClient.GetAsync(snapshotRequestUrl);
-                        HttpContent content = responseMessage.Content;
-                        using (Stream stream = await content.ReadAsStreamAsync())
-                        {
-                            var image = Image.FromStream(stream);
-                            image.Save(imagePath);
-                        }
-                        return "Take snapshot successfully: " + imagePath + ".";
-                    }
-                    else
-                    {
-                        return "Take snapshot failed, please try to input a correct image format.";
-                    }
+                    biImg.BeginInit();
+                    biImg.StreamSource = stream;
+                    biImg.EndInit();
                 }
-                else
-                {
-                    return "Take snapshot failed, please input a correct file path.";
-                }
+
+                ImageSource imgSrc = biImg as ImageSource;
+
+                return imgSrc;
             }
-            catch (Exception ex)
+            catch
             {
-                return ex.Message;
+                return null;
             }
         }
 
